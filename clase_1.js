07e79f772266cb9005eb1846dfa60874cb490c2f
Rodrigo Figueroa <jonhmordecai123@gmail.com>
2019-08-20 12:02:10 -0700

    Reto conseguido

--- clase_1.js
+++ clase_1.js
@@ -4,9 +4,19 @@ var dario = { nombre: 'Dario', apellido: 'Susnisky', edad: 27 }
 
 function imprimirNombreEnMayusculas(persona)
 {
-  var { nombre } = persona //var nombre = personba.nombre
+  var { nombre } = persona //var nombre = persona.nombre
   console.log(nombre.toUpperCase());
 }
 
 imprimirNombreEnMayusculas(sacha);
 imprimirNombreEnMayusculas(dario);
+
+function imprimirNombreYedad(persona)
+{
+var { nombre } = persona
+var { edad } = persona
+  console.log('hola ' + persona.nombre + ' tu edad es de ' + persona.edad);
+}
+
+imprimirNombreYedad(sacha);
+imprimirNombreYedad(dario);
