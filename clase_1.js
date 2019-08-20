0853071074c7e98d045e34bde624e2ddada6b9ab
Rodrigo Figueroa <jonhmordecai123@gmail.com>
2019-08-20 11:52:12 -0700

    Como se destrcutura un objto Una forma diferente

--- clase_1.js
+++ clase_1.js
@@ -2,8 +2,9 @@ var sacha = { nombre: 'sacha', apellido: 'Lifszyc', edad: 28 }//objeto
 
 var dario = { nombre: 'Dario', apellido: 'Susnisky', edad: 27 }
 
-function imprimirNombreEnMayusculas({ nombre })
+function imprimirNombreEnMayusculas(persona)
 {
+  var { nombre } = persona //var nombre = personba.nombre
   console.log(nombre.toUpperCase());
 }
 
