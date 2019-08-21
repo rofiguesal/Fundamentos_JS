f1962dd764d03de9928f06066f61c8f45e660b28
Rodrigo Figueroa <jonhmordecai123@gmail.com>
2019-08-21 10:31:42 -0700

    Uso de condicionales en JS

--- clase_1.js
+++ clase_1.js
@@ -1,9 +1,54 @@
-var x = 4, y = '4';
-
 var sacha = {
-  nombre: 'sacha'
+  nombre: 'sacha',
+  apellido: 'Sanchez',
+  edad: 28,
+  ingeniero: true,
+  cocinero: false,
+  cantante: false,
+  dj: false,
+  guitarrista: false,
+  drone: true
 }
 
-var otraPersona = {
-  nombre: 'sacha'
+function imprimirProfesiones(persona) {
+  console.log(`${persona.nombre} es:`);
+
+  if (persona.ingeniero) {
+      console.log('Ingeniero');
+  } else {
+      console.log('No es Ingeniero');
+  }
+
+  if (persona.cocinero) {
+    console.log('cocinero');
+  } else {
+    console.log('No es Cocinero');
+  }
+
+  if (persona.cantante) {
+    console.log('cantante');
+  } else {
+    console.log('No es cantante');
+  }
+
+  if (persona.dj) {
+    console.log('DJ');
+  } else {
+    console.log('No es DJ');
+  }
+
+  if (persona.guitarrista) {
+    console.log('Guitarrista');
+  } else {
+    console.log('No es Guitarrista');
+  }
+
+  if (persona.drone) {
+    console.log('Piloto de drones');
+  } else {
+    console.log('No es piloto de drones');
+  }
+
 }
+
+imprimirProfesiones(sacha);
