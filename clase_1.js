724d353789c6966a5dcc2019cf270b7cb526327e
Rodrigo Figueroa <jonhmordecai123@gmail.com>
2019-08-21 20:46:07 -0700

    Uso del loop do while

--- clase_1.js
+++ clase_1.js
@@ -1,34 +1,9 @@
-var sacha = {
-  nombre: 'sacha',
-  apellido: 'Sanchez',
-  edad: 28,
-  peso: 75
-}
+var contador = 0;
 
-console.log(`Al inicio del año ${sacha.nombre} pesa ${sacha.peso} kg`);
+const llueve = () => Math.random() < 0.25
 
-const Incremento_Peso = 0.200;
+do {
+  contador++
+} while (!llueve());
 
-const Dias_Del_Año = 365;
-
-const aumentarDePeso = persona => persona.peso += Incremento_Peso;
-
-const adelgaza = persona => persona.peso = persona.peso - Incremento_Peso;
-
-const comeMucho = () => Math.random() < 0.3;
-const realizaDeporte = () => Math.random() < 0.4;
-
-const META = sacha.peso - 3;
-var dias = 0
-
-while (sacha.peso > META) {
-  if (comeMucho()) {
-    aumentarDePeso(sacha);
-  }
-  if (realizaDeporte()) {
-    adelgaza(sacha);
-  }
-  dias += 1
-}
-
-console.log(`Pasaron ${dias} días hasta que ${sacha.nombre} adelgazó 3 kg`);
+console.log(`Fui a ver si llovia ${contador} veces`);
