349d168c80bef08a8a881a44d857b8f3138a5309
Rodrigo Figueroa <jonhmordecai123@gmail.com>
2019-08-23 11:05:40 -0700

    Uso de la function escrita pr felchas en los portotipo Es mejor utilizar Function completa

--- clase_1.js
+++ clase_1.js
@@ -11,7 +11,15 @@ Persona.prototype.saludar = function () {
   console.log(`Hola me llamo ${this.nombre} ${this.apellido}`);
 };
 
-Persona.prototype.SoyAlto = () => return this.altura > 1.8;
+//la flecha se va a referir directamnete al this que este fuera del prtotipo, se cambia quien es this.
+//Por eso nu utilizar para este tipo de cosas la flechita es mejor la function () {} entera
+//Persona.prototype.SoyAlto = () => {
+  //return this.altura > 1.8;
+//};
+
+Persona.prototype.SoyAlto = function () {
+  return this.altura > 1.8;
+};
 
 var sacha = new Persona('Sacha', 'Lifszyc', 1.82);
 var erika = new Persona('Ericka', 'Luna', 1.64);
