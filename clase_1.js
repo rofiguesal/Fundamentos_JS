b4e782cecff23d5f060af314e6638b6ec812b9a5
Rodrigo Figueroa <jonhmordecai123@gmail.com>
2019-08-23 13:00:15 -0700

    Aprendizaje de la herencia prototipal en JS, tambien la herencia no existe

--- clase_1.js
+++ clase_1.js
@@ -1,11 +1,16 @@
+function heredaDe(prototipohijo, prototipopadre) { //La herencia no existe en JS, solo hay prototipos y le herencia prototipla.
+  var fn = function () {}
+  fn.prototype = prototipopadre.prototype;
+  prototipohijo.ptototype = new fn;
+  prototipohijo.prototype.constructor = prototipohijo;
+}
+
 function Persona(nombre, apellido, altura) {
   this.nombre = nombre;
   this.apellido = apellido;
   this.altura = altura;
   return this;
 }
-
-//problema resuelto, se puso la altura e hice que retornara si eran bajos o altos.
 
 Persona.prototype.saludar = function () {
   console.log(`Hola me llamo ${this.nombre} ${this.apellido}`);
@@ -15,6 +20,18 @@ Persona.prototype.SoyAlto = function () {
   return this.altura > 1.8;
 };
 
-var sacha = new Persona('Sacha', 'Lifszyc', 1.82);
-var erika = new Persona('Ericka', 'Luna', 1.64);
-var arturo = new Persona('Arutro', 'Gimenez', 1.73);
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+}
+
+heredaDe(Desarrollador, Persona);
+
+Desarrollador.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+};
+
+
+//var sacha = new Persona('Sacha', 'Lifszyc', 1.82);
+//var erika = new Persona('Ericka', 'Luna', 1.64);
+//var arturo = new Persona('Arturo', 'Gimenez', 1.73);
