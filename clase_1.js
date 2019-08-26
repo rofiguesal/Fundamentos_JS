98fe2c6b04c5fde66a8c94dd9b4fb5445bb9c159
Rodrigo Figueroa <jonhmordecai123@gmail.com>
2019-08-26 10:35:48 -0700

    Empieza la clase

--- clase_1.js
+++ clase_1.js
@@ -1,8 +1,30 @@
-var nombre = 'Rodrigo', apellido = 'Figueroa';
-var edad = '28 años';
+ function Persona(nombre, apellido, altura) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.altura = altura;
+  return this;
+}
 
-console.log('Hola ' + nombre + ' ' + apellido);
+Persona.prototype.saludar = function () {
+  console.log(`Hola me llamo ${this.nombre} ${this.apellido}`);
+};
 
-console.log('Tengo ' + edad);
+Persona.prototype.SoyAlto = function () {
+  return this.altura > 1.8;
+};
 
-var peso = 75;
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+}
+
+heredaDe(Desarrollador, Persona);
+
+Desarrollador.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+};
+
+
+//var sacha = new Persona('Sacha', 'Lifszyc', 1.82);
+//var erika = new Persona('Ericka', 'Luna', 1.64);
+//var arturo = new Persona('Arturo', 'Gimenez', 1.73);
